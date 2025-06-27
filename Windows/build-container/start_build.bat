224f22be4bf92ec9b3b7aa4184b274b0efffc94f
Janis Kovalevskis <janis.kovalevskis@gmail.com>
2025-06-27 18:14:55 +0300

    CMake presets for community Debug/Release Windows build

--- Windows/build-container/start_build.bat
+++ Windows/build-container/start_build.bat
@@ -2,11 +2,12 @@
 
 echo "ArangoDB build on windows latest started!"
 
-git clone --depth 1 --recurse-submodules --shallow-submodules --jobs 8 --branch feature/windows-clang-build https://github.com/fceller/arangodb
+git clone --depth 1 --recurse-submodules --shallow-submodules --jobs 8 --branch feature/windows-clang-build https://github.com/fceller/arangodb src
 
-cmake --fresh -G "Visual Studio 17 2022" -B ./build-arangodb/ -S ./arangodb/  --preset community -T="ClangCL" -DSKIP_PACKAGING=TRUE -DUSE_JEMALLOC=FALSE -DUSE_LIBUNWIND=FALSE -DUSE_V8=OFF -DUSE_BUILD_ID_READER=OFF
+echo "Cloning done, now starting cmake..."
 
-cmake --build ./build-arangodb/ --target arangod.exe
+cmake --fresh -B ./build/ -S ./src/  --preset community-win
 
-echo "To build arangod target run:"
-echo "cmake --build ./build-arangodb/ --target arangod"
+echo "Build arangod..."
+
+cmake --build ./build/ --config=Release --target arangod
